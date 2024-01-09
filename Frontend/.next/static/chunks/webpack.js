631df596d9c6988cf108ecf5efafd97d7fb26cac
muhammedanaskhan <muhammedanas5186@gmail.com>
2024-01-09 15:34:50 +0530

    git ignore updatde

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "beffc433a46b4ac8"; }
+/******/ 		__webpack_require__.h = function() { return "8a648fdecd0f9a07"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
