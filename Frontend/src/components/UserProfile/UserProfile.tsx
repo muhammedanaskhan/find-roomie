62012dde8fc8ddb97247158f797d72a08f8714c0
Anas <muhammedanas5186@gmail.com>
2024-08-25 01:59:02 +0530

    city and country search in user-profile bhenkichut

--- Frontend/src/components/UserProfile/UserProfile.tsx
+++ Frontend/src/components/UserProfile/UserProfile.tsx
@@ -24,6 +24,9 @@ import { useAuthenticateUserQuery, useGetUserDataQuery, useUpdateUserDataQuery }
 import { Router, useRouter } from 'next/router'
 import Link from 'next/link'
 import { Input } from '../ui/input'
+import { Command, CommandGroup, CommandInput, CommandItem, CommandList } from '../ui/command'
+import { useDebounce } from '@/hooks/useDebounce'
+import { Skeleton } from '../ui/skeleton'
 
 
 
@@ -63,9 +66,129 @@ function UserProfile() {
   const [lastName, setLastName] = useState<string>('')
   const [username, setUsername] = useState('');
   const [gender, setGender] = useState<string | null>(null);
-  const [city, setCity] = useState<string | null>(null);
   const [preferences, setPreferences] = useState<string[] | null>(null)
 
+  const [country, setCountry] = useState<string>('')
+  const [countrySearchSuggestions, setCountrySearchSuggestions] = useState<any[]>([])
+  const [selectedCountryCode, setSelectedCountryCode] = useState<string | null>(null)
+  const [isCountrySuggestionSelected, setIsCountrySuggestionSelected] = useState<boolean>(false)
+  const [isCountrySuggestionsLoading, setIsCountrySuggestionsLoading] = useState<boolean>(false)
+
+  const handleCountryChange = (search: string) => {
+    setCountrySearchSuggestions([])
+    if (search !== '') {
+      setIsCountrySuggestionsLoading(true)
+    } else {
+      setIsCountrySuggestionsLoading(false)
+    }
+
+    setIsCountrySuggestionSelected(false)
+    setCountry(search)
+  }
+
+  const handleSelectCountrySuggestion = (name: string, countryCode: string) => {
+    setIsCountrySuggestionSelected(true)
+    setCountry(name)
+    setSelectedCountryCode(countryCode)
+  }
+
+  const debouncedCountrySearch = useDebounce(country, 500)
+
+  useEffect(() => {
+    if (debouncedCountrySearch === '' || isCountrySuggestionSelected) return
+    fetchCountryPredictions(debouncedCountrySearch)
+  }, [debouncedCountrySearch])
+
+  const fetchCountryPredictions = async (search: string) => {
+
+    console.log('Fetching country suggestions:', search);
+    const username = process.env.NEXT_PUBLIC_GEONAMES_USERNAME
+    try {
+      const response = await fetch(`http://api.geonames.org/searchJSON?q=${search}&maxRows=10&featureClass=A&featureCode=PCLI&username=${username}`)
+      const data = await response.json()
+      console.log('Country suggestions:', data);
+      const suggestions = data.geonames
+
+      setCountrySearchSuggestions(suggestions
+        .map((item: any) => {
+          return {
+            name: item.name,
+            countryCode: item.countryCode
+          }
+        })
+      )
+
+      setIsCountrySuggestionsLoading(false)
+    } catch (error) {
+      console.error('Error fetching country suggestions:', error);
+      setIsCountrySuggestionsLoading(false)
+    }
+  }
+
+  const [city, setCity] = useState<string>('')
+  const [citySearchSuggestions, setCitySearchSuggestions] = useState<any[]>([])
+  const [isCitySuggestionSelected, setIsCitySuggestionSelected] = useState<boolean>(false)
+  const [isCitySuggestionsLoading, setIsCitySuggestionsLoading] = useState<boolean>(false)
+
+  const handleCityChange = (search: string) => {
+    setCitySearchSuggestions([])
+    if (search !== '') {
+      setIsCitySuggestionsLoading(true)
+    } else {
+      setIsCitySuggestionsLoading(false)
+    }
+
+    setIsCitySuggestionSelected(false)
+    setCity(search)
+  }
+
+  const handleSelectCitySuggestion = (suggestion: string) => {
+    setIsCitySuggestionSelected(true)
+    setCity(suggestion)
+  }
+
+  const debouncedCitySearch = useDebounce(city, 500)
+
+  useEffect(() => {
+    if (debouncedCitySearch === '' || isCitySuggestionSelected) return
+    fetchCityPredictions(debouncedCitySearch)
+  }, [debouncedCitySearch])
+
+  const fetchCityPredictions = async (search: string) => {
+
+    console.log('Fetching city suggestions:', search);
+    const username = process.env.NEXT_PUBLIC_GEONAMES_USERNAME
+    try {
+      setIsCitySuggestionsLoading(true)
+      let response;
+      if (selectedCountryCode == null) {
+        response = await fetch(`http://api.geonames.org/searchJSON?q=${search}&maxRows=10&featureClass=P&username=${username}`)
+      } else {
+        response = await fetch(`http://api.geonames.org/searchJSON?q=${search}&country=${selectedCountryCode}&maxRows=10&featureClass=P&username=${username}`)
+
+      }
+      const data = await response.json()
+      console.log('City suggestions:', data);
+      const suggestions = data.geonames
+
+      setCitySearchSuggestions(suggestions
+        .map((item: any) => {
+          return {
+            name: item.name,
+            countryCode: item.countryCode
+          }
+        })
+      )
+
+    } catch (error) {
+      console.error('Error fetching country suggestions:', error);
+      setIsCountrySuggestionsLoading(false)
+    } finally {
+      setIsCitySuggestionsLoading(false)
+    }
+  }
+
+  //console.log('countrySearchSuggestions:', setCountrySearchSuggestionsName);
   const router = useRouter();
 
   const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -206,10 +329,84 @@ function UserProfile() {
             </div>
           </div>
           <div className='flex flex-col gap-4 lg:flex-row justify-between items-center'>
-            <p className='text-left w-full lg:w-min font-semibold'>City</p>
-            <div className='w-full flex gap-4 lg:w-52'>
-              <Dropdown onSelectValue={handleSelectCity} fetchedCity={city} />
+            <p className='text-left w-full lg:w-min font-semibold'>Country</p>
+            <div className="w-full relative flex gap-4 lg:w-52">
+              <Command className='border border-input !border-b-0'>
+                <CommandInput value={country} onValueChange={handleCountryChange} placeholder="Enter country..." className="h-9 " />
+                <CommandList className='absolute top-9 z-50 w-full '>
+
+                  {!isCountrySuggestionSelected && <CommandGroup>
+                    {
+                      countrySearchSuggestions.map((suggestion: any) => (
+                        <CommandItem
+                          key={suggestion.name}
+                          value={suggestion.name}
+                          onSelect={() => {
+                            handleSelectCountrySuggestion(suggestion.name, suggestion.countryCode)
+                          }}
+                          className='bg-white border border-input'
+                        >
+                          {suggestion.name}
+                        </CommandItem>
+                      ))
+                    }
+                  </CommandGroup>}
+                </CommandList>
+              </Command>
+
+              {isCountrySuggestionsLoading &&
+                <div className="w-full flex flex-col gap-3 mt-3 absolute z-50 top-9 bg-white ">
+                  <Skeleton className="h-[33.6px] w-full" />
+                  <Skeleton className="h-[33.6px] w-full" />
+                  <Skeleton className="h-[33.6px] w-full" />
+                  <Skeleton className="h-[33.6px] w-full" />
+                </div>
+              }
+
             </div>
+
+          </div>
+
+          <div className='flex flex-col gap-4 lg:flex-row justify-between items-center'>
+            <p className='text-left w-full lg:w-min font-semibold'>City</p>
+            <div className="w-full relative flex gap-4 lg:w-52">
+              <Command className='border border-input !border-b-0'>
+                <CommandInput value={city} onValueChange={handleCityChange} placeholder="Enter country..." className="h-9 " />
+                <CommandList className='absolute top-9 z-50 w-full '>
+
+
+                  {!isCitySuggestionSelected &&
+                    <CommandGroup>
+                      {
+                        citySearchSuggestions.map((suggestion: any) => (
+                          <CommandItem
+                            key={suggestion.name}
+                            value={suggestion.name}
+                            onSelect={() => {
+                              handleSelectCitySuggestion(suggestion.name)
+                            }}
+                            className='bg-white border border-input'
+                          >
+                            {suggestion.name}
+                          </CommandItem>
+                        ))
+                      }
+                    </CommandGroup>
+                  }
+                </CommandList>
+              </Command>
+
+              {isCitySuggestionsLoading &&
+                <div className="w-full flex flex-col gap-3 mt-3 absolute z-50 top-9 bg-white ">
+                  <Skeleton className="h-[33.6px] w-full" />
+                  <Skeleton className="h-[33.6px] w-full" />
+                  <Skeleton className="h-[33.6px] w-full" />
+                  <Skeleton className="h-[33.6px] w-full" />
+                </div>
+              }
+
+            </div>
+
           </div>
 
           <div className='flex flex-col justify-between items-center'>
