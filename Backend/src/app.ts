8d40bc4c466d535d2a87d660c24699fbf684b7ca
Anas <muhammedanas5186@gmail.com>
2024-08-29 12:55:32 +0530

    multiple origins

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -6,15 +6,17 @@ import bodyParser from 'body-parser';
 const app = express();
 
 // define some middlewares
+
+const allowedOrigins = process.env.CORS_ORIGIN?.split(',');
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: allowedOrigins,
     credentials: true
 }));
 
 app.use(express.json({
     limit: '50mb'
 }));
-                                            
+
 app.use(express.urlencoded({
     extended: true,
     limit: "50mb"
