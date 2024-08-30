3fce8559cff4bb15dbde6c844a0543911693a151
Anas <muhammedanas5186@gmail.com>
2024-08-30 21:24:44 +0530

    minor

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -16,6 +16,7 @@ const allowedOrigins = [
 const corsOptions = {
     origin: function (origin: any, callback: any) {
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            console.log("origin", origin)
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
