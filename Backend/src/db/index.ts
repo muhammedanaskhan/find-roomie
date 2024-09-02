99b5e86235c60ba1a475ce59cc9bdd882a26f3a6
Anas <muhammedanas5186@gmail.com>
2024-09-02 20:24:22 +0530

    docker hata dia bc

--- Backend/src/db/index.ts
+++ Backend/src/db/index.ts
@@ -4,8 +4,15 @@ import { Listing } from "../models/listing.model";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-        console.log("MondoDB connected successfully!")
+        if (!process.env.MONGO_URI) {
+            throw new Error('MONGO_URI is not defined in the environment variables');
+        }
+
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: DB_NAME
+        });
+        
+        console.log(`MongoDB connected successfully! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MOngoDB Connection error", error);
         process.exit(1);
