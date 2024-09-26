fccb50b7df30984456cd231d9900b22686c0e78e
Anas <muhammedanas5186@gmail.com>
2024-09-26 10:15:28 +0530

    minor

--- Frontend/src/components/Cards/ListingCard.tsx
+++ Frontend/src/components/Cards/ListingCard.tsx
@@ -6,19 +6,26 @@ const ListingCard: React.FC<listingCardPropsType> = (
     { userName, userAvatar, location, rent, lookingFor, currencySymbol }
 ) => {
 
+
+
+    function truncateText(text: string, maxLength: number) {
+        if (text.length <= maxLength) return text;
+        return text.slice(0, maxLength) + '...';
+    }
+
     return (
         <div className='group listingCard rounded-[16px] border max-w-[482px]  border-gray-200 px-5 py-3 hover:scale-[1.025] transition-all duration-300 ease-in-out  overflow-hidden relative'>
-            <div className="flex gap-[24px] justify-center items-center transition-all duration-300 ease-in-out ">
+            <div className="flex gap-[18px] sm:gap-[24px] justify-center items-center transition-all duration-300 ease-in-out ">
                 <Image
                     src={userAvatar}
                     alt='user'
                     width={124}
                     height={124}
                     priority={true}
-                    className='rounded-full' />
+                    className='rounded-full w-[112px] h-[112px] sm:w-[124px] sm:h-[124px]' />
                 <div className="flex flex-col gap-4">
                     <header className="flex flex-col gap-1">
-                        <h1 className="text-[18px] font-medium text-gray-600">{userName}</h1>
+                        <h1 className="text-[14px] sm:text-[18px] font-medium text-gray-600 truncate">{truncateText(userName, 18)}</h1>
                         <p className="text-[12px] truncate w-40 sm:w-80 md:w-40 lg:w-[280px]">{location}</p>
                     </header>
                     <main className="flex gap-12">
--- Frontend/src/components/Layouts/ListingSearchAreaLayout.tsx
+++ Frontend/src/components/Layouts/ListingSearchAreaLayout.tsx
@@ -3,7 +3,7 @@ import SearchArea from '../SearchArea/SearchArea'
 
 const ListingSearchAreaLayout = ({ children }: any) => {
     return (
-        <div className='mt-[60px] max-w-[1100px] flex flex-col justify-center left-0 right-0 m-auto pt-12 px-8'>
+        <div className='mt-[60px] max-w-[1100px] flex flex-col justify-center left-0 right-0 m-auto pt-8 sm:pt-12 px-4 sm:px-8'>
             <SearchArea />
             {children}
         </div>
