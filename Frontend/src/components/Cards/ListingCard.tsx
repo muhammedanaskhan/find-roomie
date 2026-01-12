097a11bbeda549f3fd3b495d58366db77c525293
Mohammad anas khan <muhammedanas5186@gmail.com>
2026-01-13 02:04:32 +0530

    hey

--- Frontend/src/components/Cards/ListingCard.tsx
+++ Frontend/src/components/Cards/ListingCard.tsx
@@ -7,7 +7,6 @@ const ListingCard: React.FC<listingCardPropsType> = (
 ) => {
 
 
-
     function truncateText(text: string, maxLength: number) {
         if (text.length <= maxLength) return text;
         return text.slice(0, maxLength) + '...';
